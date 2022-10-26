df46019e7c75fce8afe8bdf35a33d545c686daad
giulliana234 <116364106+giulliana234@users.noreply.github.com>
2022-10-26 12:04:31 -0500

    Add files via upload
    registro de alumnos

---
+++ Registro de alumnos/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.txtGrado = New System.Windows.Forms.TextBox()
+        Me.txtEdad = New System.Windows.Forms.TextBox()
+        Me.btnCarnet = New System.Windows.Forms.Button()
+        Me.pblmagen = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.pblmagen, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtNombre
+        '
+        Me.txtNombre.BackColor = System.Drawing.Color.Yellow
+        Me.txtNombre.Location = New System.Drawing.Point(104, 36)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(120, 23)
+        Me.txtNombre.TabIndex = 1
+        '
+        'txtApellido
+        '
+        Me.txtApellido.BackColor = System.Drawing.Color.Yellow
+        Me.txtApellido.Location = New System.Drawing.Point(104, 65)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(120, 23)
+        Me.txtApellido.TabIndex = 2
+        '
+        'txtGrado
+        '
+        Me.txtGrado.BackColor = System.Drawing.Color.Yellow
+        Me.txtGrado.Location = New System.Drawing.Point(104, 94)
+        Me.txtGrado.Name = "txtGrado"
+        Me.txtGrado.Size = New System.Drawing.Size(120, 23)
+        Me.txtGrado.TabIndex = 3
+        '
+        'txtEdad
+        '
+        Me.txtEdad.BackColor = System.Drawing.Color.Yellow
+        Me.txtEdad.Location = New System.Drawing.Point(104, 123)
+        Me.txtEdad.Name = "txtEdad"
+        Me.txtEdad.Size = New System.Drawing.Size(120, 23)
+        Me.txtEdad.TabIndex = 4
+        '
+        'btnCarnet
+        '
+        Me.btnCarnet.BackColor = System.Drawing.Color.White
+        Me.btnCarnet.Font = New System.Drawing.Font("Segoe UI Semibold", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.btnCarnet.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnCarnet.Location = New System.Drawing.Point(217, 173)
+        Me.btnCarnet.Name = "btnCarnet"
+        Me.btnCarnet.Size = New System.Drawing.Size(157, 69)
+        Me.btnCarnet.TabIndex = 5
+        Me.btnCarnet.Text = "Buscar"
+        Me.btnCarnet.UseVisualStyleBackColor = False
+        '
+        'pblmagen
+        '
+        Me.pblmagen.ErrorImage = CType(resources.GetObject("pblmagen.ErrorImage"), System.Drawing.Image)
+        Me.pblmagen.Image = CType(resources.GetObject("pblmagen.Image"), System.Drawing.Image)
+        Me.pblmagen.InitialImage = CType(resources.GetObject("pblmagen.InitialImage"), System.Drawing.Image)
+        Me.pblmagen.Location = New System.Drawing.Point(254, 12)
+        Me.pblmagen.Name = "pblmagen"
+        Me.pblmagen.Size = New System.Drawing.Size(134, 144)
+        Me.pblmagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pblmagen.TabIndex = 6
+        Me.pblmagen.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(29, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 21)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(27, 67)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 21)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Apellido"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(29, 94)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 21)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Grado"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(38, 125)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 21)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Edad"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI Semibold", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Location = New System.Drawing.Point(29, 173)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(159, 69)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Guardar"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(424, 275)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.pblmagen)
+        Me.Controls.Add(Me.btnCarnet)
+        Me.Controls.Add(Me.txtEdad)
+        Me.Controls.Add(Me.txtGrado)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtNombre)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Name = "Form1"
+        Me.Text = "Registro"
+        Me.TransparencyKey = System.Drawing.Color.White
+        CType(Me.pblmagen, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtCanet As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtGrado As TextBox
+    Friend WithEvents txtEdad As TextBox
+    Friend WithEvents btnCarnet As Button
+    Friend WithEvents pblmagen As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Registro de alumnos/WinFormsApp1/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar
+
+Public Class Form1
+    Dim nombre As String
+    Dim apellido As String
+    Dim grado As Integer
+    Dim edad As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        nombre = txtNombre.Text
+        apellido = txtApellido.Text
+        grado = txtGrado.Text
+        edad = txtEdad.Text
+
+        MsgBox(" Bienvenido " & nombre & " " & apellido & " " &
+           "del " & grado & " grado " & " con la edad de  " & edad & " años")
+        MsgBox("Se registro exitosamente")
+    End Sub
+
+    Private Sub btnCarnet_Click(sender As Object, e As EventArgs) Handles btnCarnet.Click
+        txtNombre.Text = ""
+        txtApellido.Text = ""
+        txtGrado.Text = ""
+        txtEdad.Text = ""
+    End Sub
+End Class
